9aef98b82f1960b305e6f85ee20344c9f7ce12f6
Danny.Song <jihun97@aricacorp.net>
2024-02-27 13:54:45 +0900

    modify 1st floor

--- Assets/cameraSwitcher.cs
+++ Assets/cameraSwitcher.cs
@@ -6,33 +6,74 @@ public class cameraSwitcher : MonoBehaviour
 {
     public Camera camera1;
     public Camera camera2;
-
+    public Camera camera3;
+    public Camera camera4;
  
+    void Start(){
+    }
+
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             camera1.enabled = true;
             camera2.enabled = false;
+            camera3.enabled = false;
+            camera4.enabled = false;
             camera1.rect = new Rect(0f, 0f, 1f, 1f);
             camera2.rect = new Rect(0f, 0f, 0f, 0f);
+            camera3.rect = new Rect(0f, 0f, 0f, 0f);
+            camera4.rect = new Rect(0f, 0f, 0f, 0f);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             camera1.enabled = false;
             camera2.enabled = true;
+            camera3.enabled = false;
+            camera4.enabled = false;
             camera1.rect = new Rect(0f, 0f, 0f, 0f);
             camera2.rect = new Rect(0f, 0f, 1f, 1f);
+            camera3.rect = new Rect(0f, 0f, 0f, 0f);
+            camera4.rect = new Rect(0f, 0f, 0f, 0f);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            camera1.enabled = false;
+            camera2.enabled = false;
+            camera3.enabled = true;
+            camera4.enabled = false;
+            camera1.rect = new Rect(0f, 0f, 0f, 0f);
+            camera2.rect = new Rect(0f, 0f, 0f, 0f);
+            camera3.rect = new Rect(0f, 0f, 1f, 1f);
+            camera4.rect = new Rect(0f, 0f, 0f, 0f);
+            
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            camera1.enabled = false;
+            camera2.enabled = false;
+            camera3.enabled = false;
+            camera4.enabled = true;
+            camera1.rect = new Rect(0f, 0f, 0f, 0f);
+            camera2.rect = new Rect(0f, 0f, 0f, 0f);
+            camera3.rect = new Rect(0f, 0f, 0f, 0f);
+            camera4.rect = new Rect(0f, 0f, 1f, 1f);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha0))
         {
             camera1.enabled = true;
             camera2.enabled = true;
-            // 첫 번째 카메라의 Viewport 설정 (화면의 왼쪽 절반)
-            camera1.rect = new Rect(0f, 0f, 0.5f, 1f);
+            camera3.enabled = true;
+            camera4.enabled = true;
 
+            // 첫 번째 카메라의 Viewport 설정 (화면의 왼쪽 절반)
+            camera1.rect = new Rect(0f, 0.5f, 0.5f, 0.5f);
+            camera2.rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
             // 두 번째 카메라의 Viewport 설정 (화면의 오른쪽 절반)
-            camera2.rect = new Rect(0.5f, 0f, 0.5f, 1f);
+            camera3.rect = new Rect(0f, 0f, 0.5f, 0.5f);
+            camera4.rect = new Rect(0.5f, 0f, 0.5f, 0.5f);
         }
         
         
---
+++ IoTObjectCommander/iotCommander.py
@@ -0,0 +1,63 @@
+import socket
+import json
+
+def create_message(choice):
+    doorID = 'front_door1'
+    evID='ev-204-1'
+    if choice == 1:
+        return {
+            "id": doorID,
+            "mType": "openDoor"
+        }
+    elif choice == 2:
+        return {
+            "id": doorID,
+            "mType": "closeDoor"
+        }
+    elif choice == 3:
+        floor = input("Enter floor: ")
+        return {
+            "id": evID,
+            "mType": "openElevatorDoor",
+            "floor": floor
+        }
+    elif choice == 4:
+        floor = input("Enter floor: ")
+        return {
+            "id": evID,
+            "mType": "closeElevatorDoor",
+            "floor": floor
+        }
+    elif choice == 5:
+        floor = input("Enter floor: ")
+        return {
+            "id": evID,
+            "mType": "moveElevator",
+            "floor": floor
+        }
+    else:
+        print("Invalid choice.")
+        return None
+
+# 서버 정보 설정
+HOST = '192.168.0.161'  # 서버 주소
+PORT = 20808        # 포트 번호
+
+# TCP 소켓 생성 및 연결
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    while True:
+        try:
+            choice = int(input("Enter your choice (1-5): "))
+            message = create_message(choice)
+            if message:
+                json_message = json.dumps(message)
+                s.send(json_message.encode('utf-8'))
+                s.send(b'\n')  # Corrected line to send newline as bytes
+                print(f"Sent message: {json_message}")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+# 연결 종료 후 클라이언트 종료
+print("Connection closed.")
